b72a1483779e52177251b2c3fa50cca3f6229115
Naveen7k <91196767+Naveen7k@users.noreply.github.com>
2021-09-23 10:16:29 +0530

    Update prac1.java

--- PracticeW3resource1/src/prac1.java
+++ PracticeW3resource1/src/prac1.java
@@ -4,5 +4,6 @@ public static void main(String[] args) {
 	Scanner in=new Scanner(System.in);
 	String name=in.nextLine();
 	System.out.println("hello"+" "+name);
+	System.out.println("edited in github");
 }
 }
